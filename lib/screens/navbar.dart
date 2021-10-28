217f94f8a765e6f31d8b67410cd53927873c3d99
Ricardo Alexandre <ricardoamjunior@gmail.com>
2021-10-27 21:39:27 -0300

    Drawer

---
+++ lib/screens/navbar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:projetodatamob/screens/navbar_parts/menu_body.dart';
+
+class Navbar extends StatefulWidget {
+  const Navbar({Key? key}) : super(key: key);
+
+  @override
+  _NavbarState createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        // Remove padding
+        padding: EdgeInsets.zero,
+        children: [
+          //Divider(),
+          MenuBody(),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () => null,
+          ),
+          ListTile(
+            leading: Icon(Icons.description),
+            title: Text('Policies'),
+            onTap: () => null,
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Exit'),
+            leading: Icon(Icons.exit_to_app),
+            onTap: () => null,
+          ),
+        ],
+      ),
+    );
+  }
+}
