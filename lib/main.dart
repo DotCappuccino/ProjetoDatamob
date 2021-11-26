7b56a1bd234fbf868a791be3fb027edba94982c3
Ricardo Alexandre <ricardoamjunior@gmail.com>
2021-11-26 00:54:06 -0300

    Integracao Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'package:projetodatamob/screens/apontamentos_parts/maps.dart';
 import 'package:projetodatamob/routes.dart';
-import 'package:projetodatamob/screens/apontamentos.dart';
 import 'package:projetodatamob/screens/login.dart';
-import 'package:projetodatamob/screens/menu.dart';
 import 'package:projetodatamob/theme.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FirebaseFirestore dataBase = FirebaseFirestore.instance;
+  CollectionReference login = dataBase.collection("login");
+
+  login.add(
+    {"nome": "TesteMasterII", "senha": "123456"},
+  ).then((value) => print("Processado com sucesso!!"));
+
   runApp(MyApp());
 }
 
